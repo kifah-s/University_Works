f31f4074d523db971176aeab7b528aa4d62c8510
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-07 19:56:44 +0200

    Add Exercise - 1

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_3_Practice_On_Compound_Data_Types/Learning/Learning.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_3_Practice_On_Compound_Data_Types/Learning/Learning.cs
@@ -1,4 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-//* >>>>>>>>>> 
+//* >>>>>>>>>> Practice On Compound Data Types <<<<<<<<<< //
+
+//* Exercise - 1 : Problem of unary matrix evaluations.
+//* The Meteorological Directorate wants to record daily rainfall rates over an entire year and make some assessments of them.
+//* required is to write a program that allows the following:
+//* - Recording daily rainfall.
+//* - Calculating the total rainfall throughout the year.
+//* - Calculate the percentage of rainy days.
+
+// //* Declaring Storage structure.
+// int[] rain_record = new int[365];
+
+// //* Declaring other Variables.
+// int sum = 0, count_rainy = 0;
+// float rainyDaysAvg;
+
+// //* Entering and Processing Values.
+// Console.WriteLine("Enter Rain Records Day by Day");
+// for (int i = 0; i < rain_record.Length; i++)
+// {
+//     rain_record[i] = Int32.Parse(Console.ReadLine());
+//     sum += rain_record[i];
+//     if (rain_record[i] > 0)
+//     {
+//         count_rainy++;
+//     }
+// }
+
+// //* Printing Results.
+// Console.WriteLine("Sum of Year Rain Quantities: " + sum);
+
+// rainyDaysAvg = (float)((count_rainy * 100) / rain_record.Length);
+// Console.WriteLine("Percent of Rainy Days: " + rainyDaysAvg);
+
+//* -------------------------------------------------------------------------
+
