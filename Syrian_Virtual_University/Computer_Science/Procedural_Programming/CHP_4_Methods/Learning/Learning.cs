1aaae7aff57b9c136b92c8426e799c785b46f907
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 17:53:02 +0200

    Add pre-defined methods

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
@@ -14,7 +14,17 @@
 
 //* Function -> A locked box.
 
-//* Example 1 :
+
+//* --------------------------------------------------------------
+
+
+//* >>>>>>>>>>> pre-defined methods <<<<<<<<<<<<<< *//
+
+//* Examples: ReadLine, WriteLine, Parse ..... etc.
+
+//* >>>> Category Math <<<< *//
+
+//* Example 1:
 /*
 const double C = 900.0;
 double x = 16.0;
@@ -23,4 +33,97 @@ Console.WriteLine(Math.Sqrt(C));
 Console.WriteLine(Math.Sqrt(x));
 Console.WriteLine(Math.Sqrt(c + d * f));
 */
+
+
+//* Example 2:
+/*
+double x, y, min, max;
+Console.WriteLine("Please enter two numbers: ");
+x = double.Parse(Console.ReadLine());
+y = double.Parse(Console.ReadLine());
+max = Math.Max(x, y);
+min = Math.Min(x, y);
+Console.WriteLine("Max of {0} and {1} is {2}", x, y, max);
+Console.WriteLine("Min of {0} and {1} is {2}", x, y, min);
+*/
+
+
+
+//* The most important methods of the Math class:
+
+//! Abs(x)
+//* The absolute value of x.
+//* Examples:
+//* Abs( 23.7 ) is 23.7
+//* Abs( 0 ) is 0
+//* Abs( -23.7 ) is 23.7
+
+
+//! Ceiling(x)
+//* Rounding to the smallest natural number not smaller than x.
+//* Examples:
+//* Cos(0.0) is 1.0
+
+
+//! Floor(x)
+//* Round to the largest natural number not greater than x.
+//* Examples:
+//* Floor( 9.2 ) is 9.0
+//* Floor( -9.8 ) is -10.0
+
+
+//! Cos(x)
+//* Examples:
+//* Cos( 0.0 ) is 1.0
+
+
+//! Sin(x)
+//* Examples:
+//* Sin( 0.0 ) is 0.0
+
+
+//! Exp(x)
+//* Raising to the power of number e.
+//* Examples:
+//* Exp( 1.0 ) is approximately .. 2.7182818284590451
+//* Exp( 2.0 ) is approximately .. 7.3890560989306504
+
+
+//! Log(x)
+//* The natural logarithm of x (base e).
+//* Examples:
+//* Log( 2.7182818284590451 ) .. is approximately 1.0
+//* Log( 7.3890560989306504 ) .. is approximately 2.0
+
+
+//! Max(x, y)
+//* Largest value.
+//* Examples:
+//* Max( 2.3, 12.7 ) is 12.7
+//* Max( -2.3, -12.7 ) is -2.3
+
+
+//! Min(x, y)
+//* Smallest value.
+//* Examples:
+//* Min( 2.3, 12.7 ) is 2.3
+//* Min( -2.3, -12.7 ) is -12.7
+
+
+//! Pow(x, y)
+//* x raised to the power y.
+//* Examples:
+//* Pow( 2.0, 7.0 ) is 128.0
+//* Pow( 9.0, .5 ) is 3.0
+
+
+//! Sqrt(x)
+//* The square root of x.
+//* Examples:
+//* Sqrt( 900.0 ) is 30.0
+//* Sqrt( 9.0 ) is 3.0
+
+
+//* --------------------------------------------------------------
+
 
