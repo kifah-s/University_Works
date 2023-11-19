1b6667061686fccbb53c2abb3c8e8a9acaaa3f5f
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-19 15:40:40 +0200

    Add notes and example

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
@@ -6,3 +6,21 @@
 //* >>>>>>>>>>>>>>>>>>>>>>>> Methods <<<<<<<<<<<<<<<<<<<<<<<<<<<< *//
 
 
+//! " divide and conquer ".
+
+//* Dividing the issue at hand into a number of partial issues.
+
+//* pre-defined methods ... AND  ... user-defined methods.
+
+//* Function -> A locked box.
+
+//* Example 1 :
+/*
+const double C = 900.0;
+double x = 16.0;
+double c = 13.0, d = 3.0, f = 4.0;
+Console.WriteLine(Math.Sqrt(C));
+Console.WriteLine(Math.Sqrt(x));
+Console.WriteLine(Math.Sqrt(c + d * f));
+*/
+
