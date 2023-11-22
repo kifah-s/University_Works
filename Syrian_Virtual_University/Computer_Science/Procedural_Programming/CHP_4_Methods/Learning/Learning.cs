bce91ab28a3082149d2e8e1a727d42733d53ba91
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-22 13:41:29 +0300

    Add Example

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_4_Methods/Learning/Learning.cs
@@ -407,6 +407,75 @@ Console.WriteLine("S = " + S);
 
 
 
+/*
+//* .............. Functions ..............
+
+
+//* parameter x receives a copy of the value passed as an argument,
+//* so this method cannot modify the caller's variable.
+static void Square(int x)
+{
+    x = x * x;
+} //* end method Square
+
+
+
+//* uses reference parameter x to modify caller's variable.
+static void SquareRef(ref int x)
+{
+    x = x * x; //* squares value of caller's variable.
+
+} //* end method SquareRef.
+
+
+//* uses output parameter x to assign a value to an uninitialized variable.
+static void SquareOut(out int x)
+{
+    x = 6; //* assigns a value to caller's variable.
+    x = x * x; //* squares value of caller's variable.
+} //* end method SquareOut.
+
+
+//* .............. End Functions ..............
+
+
+//* call methods with value, reference and output parameters.
+int y = 5; //* initialize y to 5.
+int z; //* declares z, but does not initialize it.
+
+//* --------------------------
+
+//* display original values of y and z.
+Console.WriteLine("Original value of y: {0}", y);
+Console.WriteLine("Original value of z: uninitialized\n");
+
+//* --------------------------
+
+//* pass y and z by value.
+Square(y);
+//* Square(z); //* Error.
+
+//* display values of y and z after they are passed to method Square,
+//* to demonstrate arguments passed by value are not modified.
+Console.WriteLine("Value of y after Square: {0}", y);
+//* Console.WriteLine("Value of z after Square: {0}", z); //* Error.
+
+System.Console.WriteLine(); //* --------------------------
+
+//* pass y and z by reference.
+SquareRef(ref y); //* must use keyword ref.
+SquareOut(out z); //* must use keyword out.
+
+//* display values of y and z after they are modified by,
+//* methods SquareRef and SquareOut, respectively.
+Console.WriteLine("Value of y after SquareRef: {0}", y);
+Console.WriteLine("Value of z after SquareOut: {0}\n", z);
+*/
+
+
+
+
+
 
 
 
