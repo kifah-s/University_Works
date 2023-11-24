435a2a630344c30175122cad64a9e9dc9e49fe14
kifah saloum <kifah.saloum.96@hotmail.com>
2023-11-24 20:17:58 +0300

    Add Exam

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/Exam/Exam/Exam.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/Exam/Exam/Exam.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\nHello Doctor Hwaida");
+Console.WriteLine("\nHello Doctor Hwaida\n");
 Console.WriteLine("I'm kifah (ID: kifah_134765), and this is my HW:\n");
+Console.WriteLine("........ The first problem: sorting and arranging arrays ........\n");
 
 
 //* >>>>>>>>>> Functions <<<<<<<<<< *//
@@ -38,10 +39,9 @@ static void PrintArray(int[] arr)
 }
 
 
-//* ........... Algorithms Functions ...........
+//! ........... Algorithms Functions ...........
 
-
-// Bubble Sort function.
+// 1 - Bubble Sort function.
 static void BubbleSort(int[] arr)
 {
     int n = arr.Length;
@@ -61,12 +61,75 @@ static void BubbleSort(int[] arr)
         }
     }
 }
+// End Bubble Sort function.
 
+// 2 - Merge Sort function.
+static void MergeSort(int[] array)
+{
+    if (array.Length <= 1)
+        return;
 
+    int middle = array.Length / 2;
+    int[] left = new int[middle];
+    int[] right = new int[array.Length - middle];
 
+    // Copy data to temporary arrays left[] and right[]
+    Array.Copy(array, 0, left, 0, middle);
+    Array.Copy(array, middle, right, 0, array.Length - middle);
 
+    // Recursively sort the sub-arrays
+    MergeSort(left);
+    MergeSort(right);
+
+    // Merge the sorted halves
+    Merge(array, left, right);
+}
+
+static void Merge(int[] array, int[] left, int[] right)
+{
+    int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+
+    // Compare elements from left and right arrays and merge them
+    while (leftIndex < left.Length && rightIndex < right.Length)
+    {
+        if (left[leftIndex] <= right[rightIndex])
+        {
+            array[mergedIndex] = left[leftIndex];
+            leftIndex++;
+        }
+        else
+        {
+            array[mergedIndex] = right[rightIndex];
+            rightIndex++;
+        }
+        mergedIndex++;
+    }
+
+    // Copy remaining elements from left, if any
+    while (leftIndex < left.Length)
+    {
+        array[mergedIndex] = left[leftIndex];
+        leftIndex++;
+        mergedIndex++;
+    }
+
+    // Copy remaining elements from right, if any
+    while (rightIndex < right.Length)
+    {
+        array[mergedIndex] = right[rightIndex];
+        rightIndex++;
+        mergedIndex++;
+    }
+}
+// End Merge Sort function.
+
+//! ........... End Algorithms Functions ...........
 
 //* >>>>>>>>>> End Functions <<<<<<<<<< *//
+
+
+
+Console.WriteLine("1 - Bubble Sort Algorithm ..\n");
 
 // Create random array.
 int[] randomArray1 = CreateRandomArray();
@@ -86,8 +149,22 @@ PrintArray(randomArray1);
 Console.WriteLine("\n-------------------------------------------\n");
 
 
+Console.WriteLine("2 - Merge Sort Algorithm ..\n");
+
+// Create random array.
+int[] randomArray2 = CreateRandomArray();
+
+// Print original array - (Print array before sorted by merge sort algorithm).
+Console.WriteLine("Original Array:");
+PrintArray(randomArray2);
+
+// Call the merge Sort function
+MergeSort(randomArray2);
+
+// Print array after sorted by merge sort algorithm
+Console.WriteLine("\nSorted Array By Merge Sort Algorithm:");
+PrintArray(randomArray2);
 
 
-
-
+Console.WriteLine("\n-------------------------------------------\n");
 
