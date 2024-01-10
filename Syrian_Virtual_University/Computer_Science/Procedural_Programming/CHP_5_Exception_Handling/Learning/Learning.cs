6d564c512ed8f392f54b1ce7b941cadd12732da4
kifah saloum <kifah.saloum.96@hotmail.com>
2024-01-10 13:00:26 +0300

    Add Predefined exception classes

--- Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_5_Exception_Handling/Learning/Learning.cs
+++ Syrian_Virtual_University/Computer_Science/Procedural_Programming/CHP_5_Exception_Handling/Learning/Learning.cs
@@ -442,3 +442,80 @@ finally
 //* Re-try with a different numbers.
 */
 
+
+
+/*
+//* Keyword "throw":
+
+//* C# allows you to define your own exceptions and restrictions and throw exceptions if they are violated.
+//* Exceptions can be thrown manually using the throw keyword,
+//*     Therefore, any exception derived from the Exception class can be thrown manually using this keyword.
+
+//* Example:
+
+//* Functions ..
+static void PrintMark(int m)
+{
+    if (m < 0)
+    {
+        throw new Exception("Mark must be positive");
+        //* When the "throw keyword" is executed,
+        //*     the function is stopped from executing the rest of the commands that follow it.
+    }
+
+    Console.WriteLine(m);
+
+}
+//* End Functions ..
+
+int mark = -10;
+try
+{
+    PrintMark(mark);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//* Output: Mark must be positive
+*/
+
+
+
+/*
+//* Predefined exception classes:
+
+
+//* Exception Class:                                Description:
+//* ArgumentException                               Raised when a non-null argument that is passed to a method is invalid.
+
+//* ArgumentNullException                           Raised when null argument is passed to a method.
+
+//* ArgumentOutOfRangeException                     Raised when the value of an argument is outside the range of valid values.
+
+//* DivideByZeroException                           Raised when an integer value is divide by zero.
+
+//* FileNotFoundException                           Raised when a physical file does not exist at the specified location.
+
+//* FormatException                                 Raised when a value is not in an appropriate format to be converted from,
+//*                                                 a string by a conversion method such as Parse.
+
+//* IndexOutOfRangeException                        Raised when an array index is outside the lower or upper bounds of an array or collection.
+
+//* InvalidOperationException                       Raised when a method call is invalid in an object's current state.
+
+//* KeyNotFoundException                            Raised when the specified key for accessing a member in a collection is not exists.
+
+//* NotSupportedException                           Raised when a method or operation is not supported.
+
+//* NullReferenceException                          Raised when program access members of null object.
+
+//* OverflowException                               Raised when an arithmetic, casting, or conversion operation results in an overflow.
+
+//* OutOfMemoryException                            Raised when a program does not get enough memory to execute the code.
+
+//* StackOverflowException                          Raised when a stack in memory overflows.
+
+//* TimeoutException                                The time interval allotted to an operation has expired.
+*/
